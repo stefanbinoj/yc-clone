7c3a0ce313e3cbf304f97855c00586395f45db64
stefanbinoj <stefan.binoj.007@gmail.com>
2024-12-12 18:34:36 +0530

    day 1

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,31 @@
 import NextAuth from "next-auth";
-import { authOptions } from "./options";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+  ],
+  secret: process.env.NEXT_AUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        //session.user._id = token._id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+};
 
 const handler = NextAuth(authOptions);
 
--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -1,38 +1,60 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { useSession, signIn, signOut } from "next-auth/react"
-import { getServerSession } from "next-auth/next";
-import { authOptions } from '../api/auth/[...nextauth]/options';
+import Link from "next/link";
+import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { signIn , signOut } from "next-auth/react";
 
-const Navbar = async() => {
-    const session = await getServerSession(authOptions);
-    return (
-    <header className='shadow-xl font-work-sans bg-white text-black px-5 py-3 text-xl'>
-        <nav className='flex justify-betweeen item-center' >
-            <Link href="/">
-                <Image src="/logo.png" alt="logo" height={30} width={144} />
-            </Link>
+const Navbar = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session)
 
-            <div className='flex item-center gap-5'>
-                {session && session.user? (
-                    <>
-                        <Link href='/startup/create'><span>create</span></Link>
-                        <button onClick={signOut} ><span>sign out</span></button>
-                        <Link href={`/user/${session?._id}/`}><span>{session?.user?.name}</span></Link>
-                    </>
-                ) : (
-                <>
-                    
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
 
-                </>)
-                }
-            </div>
-        </nav>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span className="max-sm:hidden">Create</span>
+                
+              </Link>
 
-    
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                  
+                </button>
+              </form>
+
+              <Link href={`/user/${session?.id}`}>
+                pfp
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
